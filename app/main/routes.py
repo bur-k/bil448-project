25474787130f7571fb9fad168aa7934f9276ab8c
Burak <36450327+bur-k@users.noreply.github.com>
2020-04-06 23:55:31 +0300

    Update routes.py

--- app/main/routes.py
+++ app/main/routes.py
@@ -22,23 +22,23 @@ def index(): # index page, the displayed page when localhost:5000/ loaded
 @main.route('/passwordCheck', methods=['GET', 'POST'])
 def passwordCheck(): # 
     form = PasswordForm()
-    user = getUser(session['username'])[1] # get 
+    user = getUser(session['username'])[1] # get user with given username
     if form.validate_on_submit():
-        if "challenge" in session.keys():
-            response = decryptAES(session['challenge'], form.response.data).decode()
+        if "challenge" in session.keys(): # if challenge exists in session
+            response = decryptAES(session['challenge'], form.response.data).decode() # dexrypt it
             json_resp = json.loads(response)
             room = int(json_resp['room'])
             session['session_key'] = json_resp['session_key']
-            if room in range(0, 10):
+            if room in range(0, 10): # if room value from loaded json that client sent is between 0-10, otherwise it means client could not pass challenge response
                 session['room'] = room
-                return redirect(url_for('.chat'))
+                return redirect(url_for('.chat')) # redirect it to chat page
     elif request.method == 'GET' or request.method == 'POST':
         passwordHash = user['passwordHash']
         form.salt = user['salt']
-        data = hashlib.sha256(bcrypt.gensalt()).hexdigest()[:64]
+        data = hashlib.sha256(bcrypt.gensalt()).hexdigest()[:64] # create challenge
         session['challenge'] = data
         key = hashlib.sha256(passwordHash.encode()).hexdigest()[:64]
-        form.challenge = encryptAES(key, data).decode()
+        form.challenge = encryptAES(key, data).decode() # encrypt challenge with hash of user password
     return render_template('passwordCheck.html', form=form)
 
 
